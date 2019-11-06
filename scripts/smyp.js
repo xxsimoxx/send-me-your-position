fd3f0609fb304db7948e4af46f03f318af043950
xxsimoxx <simospa@gieffeedizioni.it>
2019-11-06 14:33:59 +0100

    basic structure

---
+++ scripts/smyp.js
@@ -0,0 +1,29 @@
+var destphone="+393473650691";
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(sendPosition);
+	} else {
+		console.log("Geolocation is not supported by this browser.");
+	}
+}
+
+function smypSend(){
+	console.log("Button");
+}
+
+function sendPosition(position) {
+	var person = prompt("Please enter your name.");
+	var mapsUrl="https://www.google.com/maps/search/?api=1&pquery=" + position.coords.latitude+","+position.coords.longitude;
+	var message=encodeURIComponent(person+"\n");
+	var WAapi = "https://api.whatsapp.com/send?phone="+destphone+"&text="+message+encodeURIComponent(mapsUrl);
+	window.open(WAapi,'_self',false);
+}
+
+function canLocate(){
+	navigator.permissions.query({ name: 'geolocation' }).then(console.log)
+}
+
+window.onload = function() {
+  canLocate();
+};
--- send-me-your-position.php
+++ send-me-your-position.php
@@ -2,54 +2,56 @@
 
 /**
  * -----------------------------------------------------------------------------
- * Plugin Name: Utility Plugin - Namespaced OO
- * Description: A better solution to placing code-snips in the theme's functions.php file. This version of the plugin contains namespaced, object-oriented code.
- * Version: 1.0.0
- * Author: Code Potent
- * Author URI: https://codepotent.com
- * Plugin URI: https://codepotent.com/classicpress-plugins/
- * Text Domain: codepotent-utility-plugin-namespaced-oo
+ * Plugin Name: Send me your position
+ * Description: ClassicPress plugin to add a button that sends your position via WhatsApp
+ * Version: 0.0.1
+ * Author: Gieffe edizioni srl
+ * Author URI: https://www.gieffeedizioni.it/classicpress
+ * Plugin URI: https://github.com/xxsimoxx/send-me-your-position
+ * Text Domain: smyp
  * Domain Path: /languages
+ * GitHub Plugin URI: xxsimoxx/send-me-your-position
  * -----------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Full
  * text of the license is available at https://www.gnu.org/licenses/gpl-2.0.txt.
  * -----------------------------------------------------------------------------
- * Copyright © 2019 - CodePotent
- * -----------------------------------------------------------------------------
- *           ____          _      ____       _             _
- *          / ___|___   __| | ___|  _ \ ___ | |_ ___ _ __ | |_
- *         | |   / _ \ / _` |/ _ \ |_) / _ \| __/ _ \ '_ \| __|
- *         | |__| (_) | (_| |  __/  __/ (_) | ||  __/ | | | |_
- *          \____\___/ \__,_|\___|_|   \___/ \__\___|_| |_|\__|.com
- *
- * -----------------------------------------------------------------------------
- * Purpose
- *
- * This plugin provides a container for code-snips that might otherwise be added
- * to your theme's functions.php file. You can treat this file as though it were
- * your functions.php file – just add your code-snips here instead. Placing your
- * code-snips here also allows you to activate and deactivate them, the same way
- * you can with any plugin. For more information, see: https://bit.ly/2VSE6k3
+ * Copyright © 2019 - Simone Fioravanti
  * -----------------------------------------------------------------------------
  */
 
-namespace YourOwnPrefix\UtilityPlugin;
+namespace xxsimoxx\smyp;
 
-class Utility {
+defined( 'ABSPATH' ) || exit;
 
-	// Add actions and filters to this method.
+class Send_Position {
+
 	public function __construct() {
-		add_filter('admin_footer_text', [$this, 'custom_footer_text']);
+		add_action( 'wp_enqueue_scripts', [$this, 'register_script'] );
+		add_shortcode( 'smyp', [$this, 'button_shortcode'] );
+		
 	}
 
-	// A function that returns a string of custom text.
-	public function custom_footer_text($text) {
-		$text = esc_html__('Built with endorphins and adrenaline!', 'codepotent-utility-plugin-namespaced-oo');
-		return $text;
+	public function register_script() {
+		wp_register_script( 'smyp-script', plugins_url( '/scripts/smyp.js', __FILE__ ), array(), '1.0.0', false  );
+		wp_register_style( 'smyp-style', plugins_url( '/styles/smyp.css', __FILE__ ), array(), '1.0.0', 'all'  );
+
+		//		echo"<script>console.log('register')</script>";
+
+	}
+
+
+	public function button_shortcode( $atts, $content = null ) {
+		wp_enqueue_script('smyp-script');
+		wp_enqueue_style('smyp-style');
+		//if( ! wp_style_is( "smyp-style", $list = 'enqueued' ) ) { wp_enqueue_style('smyp-style'); }
+
+		//if( ! wp_style_is( "smyp-style", $list = 'enqueued' ) ) { wp_enqueue_style('smyp-style'); }
+		//if( ! wp_script_is( "smyp-script", $list = 'enqueued' ) ) { wp_enqueue_script('smyp-script'); }
+		return '<button class="smyp-button" onclick="smypSend()">' . $content .'</button>';
 	}
 
 }
 
-new Utility;
+new Send_Position;
